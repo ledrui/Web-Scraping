4232df0291a51be35c2c906a78eaf629c28a0c5a
Iliass <tiiliass@gmail.com>
2015-09-16 23:20:15 -0700

    error chaching

--- scraptest.py
+++ scraptest.py
@@ -1,6 +1,26 @@
+#!scrapEnv/bin/python3
 from urllib.request import urlopen 
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
+def getTitle(url):
+	try:
+		html = urlopen(url)
+	except HTTPError as e:
+		print('HTTPError')
+		return None
 
-html = urlopen("http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world")
-bsObj = BeautifulSoup(html.read())
-print (bsObj.h1)
+	try:
+		bsObj = BeautifulSoup(html.read()) 
+		title = bsObj.body.h1
+	except AttributError as e:
+		print('AttributError')
+		return None
+
+	return title
+
+title = getTitle("http://webapplog.com/test-driven-development-in-node-js-with-mocha")
+
+if title == None:
+	print("Title could not be found")
+else:
+	print(title)
